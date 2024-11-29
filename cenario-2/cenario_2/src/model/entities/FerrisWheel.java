27dd3c5e588c3e7e1ca2aa608e218e1d25ab8fb8
Joao_Gabriel <jgcarvalho16@outlook.com>
2024-11-29 14:55:07 -0300

    Declaring a constant MAX_GONDOLAS with the value 18, the number of gondolas on the ferris wheel.

--- cenario-2/cenario_2/src/model/entities/FerrisWheel.java
+++ cenario-2/cenario_2/src/model/entities/FerrisWheel.java
@@ -2,14 +2,17 @@ package model.entities;
 
 public class FerrisWheel {
 
-    private Gondola[] gondolas = new Gondola[18];
+    private static final int MAX_GONDOLAS = 18;
+    private Gondola[] gondolas = new Gondola[MAX_GONDOLAS];
 
+    // Constructor that initializes the gondolas, creating a new gondola for each index up to the maximum limit MAX_GONDOLAS
     public FerrisWheel() {
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < MAX_GONDOLAS; i++) {
             gondolas[i] = new Gondola(i+1);
         }
     }
 
+    // Method to display the gondolas and their respective passengers
     public void status(){
         System.out.println("* Gondola Status");
         System.out.println("* --------------------");
