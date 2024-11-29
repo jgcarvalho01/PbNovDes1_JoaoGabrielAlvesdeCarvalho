84dc5b36e04d74b8cdb0237cb63cb9155133294c
Joao_Gabriel <jgcarvalho16@outlook.com>
2024-11-29 14:05:53 -0300

    Implementing the classes Person, Adult, and Child, and creating the method in the Child class to get the responsible adult.

--- cenario-2/cenario_2/src/application/Main.java
+++ cenario-2/cenario_2/src/application/Main.java
@@ -1,7 +1,17 @@
 package application;
 
+import model.entities.Adult;
+import model.entities.Child;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Adult gabs = new Adult("Gabs", 25);
+        Child dante = new Child("Dante", 5, gabs);
+
+        System.out.println(gabs);
+        System.out.println(dante);
+        System.out.println(dante.getResponsible());
+
     }
 }
--- cenario-2/cenario_2/src/model/entities/Child.java
+++ cenario-2/cenario_2/src/model/entities/Child.java
@@ -1,4 +1,22 @@
 package model.entities;
 
-public class Child {
+public class Child extends Person{
+
+    private Adult responsible;
+
+    public Child(String name, int age){
+        super(name, age);
+    }
+
+    // Constructor of the Child class that initializes the name, age, and responsible adult for the child
+    public Child(String name, int age, Adult responsible) {
+        super(name, age);
+        this.responsible = responsible;
+    }
+
+    // Method that returns the responsible adult for the child
+    public Adult getResponsible() {
+        return responsible;
+    }
+
 }
