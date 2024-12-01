1754a69fb43a38806d3cd73bb2e66770ee020152
Joao_Gabriel <jgcarvalho16@outlook.com>
2024-12-01 13:13:23 -0300

    Implementing the emoji counter in the sentence.

--- cenario-extra/cenario_extra/src/Main.java
+++ cenario-extra/cenario_extra/src/Main.java
@@ -5,12 +5,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Entrada: ");
         String sentence = sc.nextLine();
-        if(sentence.contains(":-)")){
-            System.out.println("divertido");
-        } else if(sentence.contains(":-(")){
-            System.out.println("chateado");
-        } else {
-            System.out.println("neutro");
+
+        int position = 0;
+        int count_funny = 0;
+        int count_sad = 0;
+
+        while ((position = sentence.indexOf(":-)", position)) != -1) {
+            count_funny++;
+            position += ":-)".length();
         }
+        System.out.println("Number of funny: " + count_funny);
+
+        position = 0;
+        while ((position = sentence.indexOf(":-(", position)) != -1) {
+            count_sad++;
+            position += ":-(".length();
+        }
+        System.out.println("Number of sad: " + count_sad);
+
+//        if(sentence.contains(":-)")){
+//            System.out.println("divertido");
+//        } else if(sentence.contains(":-(")){
+//            System.out.println("chateado");
+//        } else {
+//            System.out.println("neutro");
+//        }
     }
 }
